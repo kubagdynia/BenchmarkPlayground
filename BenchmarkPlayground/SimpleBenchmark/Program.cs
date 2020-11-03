cd0e4a130b097e74cebe13bd87cf533478e14cf3
Jakub Kurłowicz <kubagdynia@gmail.com>
2020-11-03 23:06:39 +0100

    Removed redundant code

--- BenchmarkPlayground/SimpleBenchmark/Program.cs
+++ BenchmarkPlayground/SimpleBenchmark/Program.cs
@@ -7,42 +7,27 @@ namespace SimpleBenchmark
     public class EnumTests
     {
         private const OperationStatus Status = OperationStatus.Success;
-        private const OperationType Type = OperationType.Update;
 
         [Benchmark]
         public void EnumToString()
-        {
-            Type.ToString();
-            Status.ToString();
-        } 
+            => Status.ToString();
 		
         [Benchmark]
         public void EnumToStringCode()
-        {
-            Type.ToStringCode();
-            Status.ToStringCode();
-        }
+            => Status.ToStringCode();
         
         [Benchmark]
         public void EnumToStringCodeUpperCaseUsingToUpper()
-        {
-            Type.ToStringCode().ToUpper();
-            Status.ToStringCode().ToUpper();
-        }
+            => Status.ToStringCode().ToUpper();
 		
         [Benchmark]
         public void EnumToStringCodeUpperCaseUsingExtension()
-        {
-            Type.ToStringCodeUpperCase();
-            Status.ToStringCodeUpperCase();
-        }
+            => Status.ToStringCodeUpperCase();
     }
 
     internal class Program
     {
         public static void Main(string[] args)
-        {
-            var summary = BenchmarkRunner.Run<EnumTests>();
-        }
+            => BenchmarkRunner.Run<EnumTests>();
     }
 }
